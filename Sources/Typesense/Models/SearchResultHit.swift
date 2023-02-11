81b0c32ae1f76428d910e8e3d3bee035c2a33b26
Brandon <mauzybwy@gmail.com>
2023-02-10 23:10:41 -0500

    Support vector_distance results

--- Sources/Typesense/Models/SearchResultHit.swift
+++ Sources/Typesense/Models/SearchResultHit.swift
@@ -18,12 +18,14 @@ public struct SearchResultHit<T: Codable>: Codable {
     public var textMatch: Int64?
     /** Can be any key-value pair */
     public var geoDistanceMeters: [String:Int]?
+    public var vectorDistance: String?
 
-    public init(highlights: [SearchHighlight]? = nil, document: T? = nil, textMatch: Int64? = nil, geoDistanceMeters: [String:Int]? = nil) {
+    public init(highlights: [SearchHighlight]? = nil, document: T? = nil, textMatch: Int64? = nil, geoDistanceMeters: [String:Int]? = nil, vectorDistance? = nil) {
         self.highlights = highlights
         self.document = document
         self.textMatch = textMatch
         self.geoDistanceMeters = geoDistanceMeters
+        self.vectorDistance = vectorDistance
     }
 
     public enum CodingKeys: String, CodingKey { 
@@ -31,6 +33,7 @@ public struct SearchResultHit<T: Codable>: Codable {
         case document
         case textMatch = "text_match"
         case geoDistanceMeters = "geo_distance_meters"
+        case vectorDistance = "vector_distance"
     }
 
 }
